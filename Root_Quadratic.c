ad0a39cf031cb88dea9d32ad86384e54883cff81
Susheel Thapa <susheel.thapa@outlook.com>
2021-06-08 08:28:30 +0545

    Error in calculating the complex root is fixed

--- Root_Quadratic.c
+++ Root_Quadratic.c
@@ -20,7 +20,7 @@ Formula used:
 int main()
 {
     //Declaring the required variable
-    float a, b, c, discriminant, root_one, root_two;
+    float a, b, c, discriminant, root_one_real, root_two_real, root_imaginary;
 
     //Below lines are just to given a brief knowledge on how to input the value to the user...
     system("cls");
@@ -33,6 +33,8 @@ int main()
 
     //Main code
     system("cls");
+
+    //Taking input from the user
     printf("Enter the value of a,b,c:\n");
     scanf("%f %f %f", &a, &b, &c);
 
@@ -41,27 +43,32 @@ int main()
     {
         //Calculating the discriminant
         discriminant = pow(b, 2) - 4 * a * c; // Discriminant = b^2 - 4ac
+
         //Below if check whether the root is imaginary or real root.
         if (discriminant >= 0)
         {
             // Calculating roots usingx = −b ± √ (dicriminant)* 1 / 2a
-            root_one = (-b + pow(discriminant, 0.5)) / (2 * a);
-            root_two = (-b - pow(discriminant, 0.5)) / (2 * a);
-            printf("\nThe root are %.2f and %.2f.", root_one, root_two);
+            root_one_real = (-b + pow(discriminant, 0.5)) / (2 * a);
+            root_two_real = (-b - pow(discriminant, 0.5)) / (2 * a);
+
+            printf("\nRoot One : %.2f \nRoot Two : %.2f", root_one_real, root_two_real);
         }
         else if (discriminant < 0)
         {
-            //Here discriminant is changeed to positive and used  x = −b ± √ (dicriminant)* 1 / 2a
+            //Here discriminant is changeed to positive and real part and imaginary part are calculated seperatly and merged while printing the root.
             discriminant = -discriminant;
-            root_one = (-b + pow(discriminant, 0.5)) / (2 * a);
-            root_two = (-b + pow(discriminant, 0.5)) / (2 * a);
-            printf("\nThe root are %.2fi and %.2fi.", root_one, root_two);
+            root_one_real = (-b) / (2 * a);
+            root_two_real = (-b) / (2 * a);
+            root_imaginary = (pow(discriminant, 0.5)) / (2 * a);
+            
+            printf("\nRoot One : %.2f + %.2fi \nRoot Two : %.2f - %.2fi", root_one_real, root_imaginary, root_two_real, root_imaginary);
         }
     }
     else
     {
-        printf("\nYou didnot give correct quadritic equation.");
+        printf("\nYou didnot give correct quadratic equation.");
     }
+
     getch(); //Wait till a character is pressed
     return 0;
 }
