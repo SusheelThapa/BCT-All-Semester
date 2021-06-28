9229bb36903b6098b0ec43c8dbb9171c2d6986d4
Susheel Thapa <susheel.thapa@outlook.com>
2021-06-29 00:54:47 +0545

    Comment are added for Program 5.1 of Lab 6

--- Computer Lab/Lab 6 Source Code/Program05_01.c
+++ Computer Lab/Lab 6 Source Code/Program05_01.c
@@ -1,19 +1,41 @@
-/* 1,2,3,4,5,6...................n*/
+/* 
+Question:
+    1,2,3,4,5,6...................n
+    
+*/
+
+
+/*Importing Header Files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 
+
 int main()
 {
+    /*Varaible Declaration*/
     int n;
-    system("cls");
+
+    system("cls"); /*Clear the terminal Screen*/
+
+    /*Taking input from user*/
     printf("Value of n: ");
     scanf("%d", &n);
-    printf("\nNumber for 1 to n is listed below: \n\n", n);
+
+    printf("\nNumber for 1 to n is listed below: \n\n", n); //Information
+
+    /*Simple for loop to print form 1 to n*/
     for (int i = 1; i <= n; i++)
     {
         printf("%d ", i);
     }
-    getch();
+
+    getch(); //Waits till a character is pressed*/
     return 0;
 }
+/*
+Logic of code:
+
+    We just use simple for loop from 1 to n and print all the value from 1 to n.
+
+*/
