d416698350e7c089b7f2b10cb4a740c7d5ea2da0
Susheel Thapa <susheel.thapa@outlook.com>
2021-06-29 11:57:13 +0545

    Comments are added for Program 7

--- Computer Lab/Lab 6 Source Code/Program07.c
+++ Computer Lab/Lab 6 Source Code/Program07.c
@@ -1,6 +1,10 @@
 /* 
-1+ x/1! +x^2/2! + x^3/3!.......... till sum of terms is less thab 10^-6
+Questions:
+    1+ x/1! +x^2/2! + x^3/3!.......... till sum of terms is less thab 10^-6
+
 */
+
+/*Importing header files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
@@ -8,22 +12,55 @@
 
 int main()
 {
+    /*Variable Declaration*/
     int power =0;
     double x, term = 1, sum_of_term = 0,condition;
-    system("cls");
+
+    system("cls");/*Clear the screen*/
+
+    /*Taking user input*/
     printf("Value of x: ");
     scanf("%lf", &x);
-    printf("\n\nSequence with value of x till term>10^-6: \n");
+
+    printf("\n\nSequence with value of x till term>10^-6: \n");/*Information*/
+
+    /*While loop and it exit whenever condition is <=0*/
     while(condition >(double)0){
+
         term = pow(x, power) / tgamma(power + 1);
-        printf("%.10f +", term);
-        sum_of_term = term + sum_of_term;
-        condition=term - 0.000001;
-        power++;
+
+        printf("%.10f +", term);/*Print the term*/
+
+        sum_of_term = term + sum_of_term; /*Calculate the sum of term*/
+        condition=term - 0.000001; /*Calculate the condition*/
+        power++; /*Increase the power*/
     }
+
+    /*Meaningful Result*/
     printf("\n\nSum of term : %.5f\n", sum_of_term);
     printf("\nAt %d term,term is less than 10^-6\n", power);
 
-    getch();
+    getch();/*Wait till key is presssed*/
     return 0;
 }
+/*
+We use formula fof t(n) term:
+    t(n) = x^0/0!
+    We use tgamma function to calculate the factorial of number
+        SYNTAX:
+            tgamma(n)= (n-1)!
+        
+    We use pow function for calculating power of x
+        SYNTAX:
+            pow(x,power)
+
+    On each iteration we increased the value of power
+        power++
+
+    Now we have given condition to run while loop in questions:
+        term>10^-6
+        term-10^-6>0
+        condition= term-10^-6>0
+
+    Compile all these in code:
+*/
