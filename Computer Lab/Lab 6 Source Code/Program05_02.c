7cb1d07c8a22d314a60ecda21361a8b0fa5bb1d2
Susheel Thapa <susheel.thapa@outlook.com>
2021-06-29 10:56:07 +0545

    Comments are added for Program5.2

--- Computer Lab/Lab 6 Source Code/Program05_02.c
+++ Computer Lab/Lab 6 Source Code/Program05_02.c
@@ -1,20 +1,40 @@
-/* 2,4,6,8,10 ..................,2n*/
+/* 
+Questions:
+    2,4,6,8,10 ..................,2n
+
+*/
+
+/*Importing header files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 
 int main()
 {
+    /*Variable Declaration*/
     int n;
-    system("cls");
+
+    system("cls");/*Clear the screen*/
+
+    /*Taking input from user*/
     printf("Value of n: ");
     scanf("%d", &n);
-    printf("\nNumber for 2 to 2n is listed below: \n\n", n);
+
+    printf("\nNumber for 2 to 2n is listed below: \n\n", n); /*Imformation*/
+
+    /*Simple for loop*/
     for (int i = 1; i <= n; i++)
     {
-        printf("%d ", 2 * i);
+        printf("%d ", 2 * i); /*Printing value of i multiply by 2*/
     }
 
     getch();
     return 0;
 }
+/*
+Logic of code:
+
+    We just use single for loop to iterate from 1 to n
+    ~write printing we jsut multiply i by 2 to get series of even number
+
+*/
