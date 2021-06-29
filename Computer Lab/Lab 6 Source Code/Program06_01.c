88c1043a0de3bd36a9f6419185d4ec40957e0189
Susheel Thapa <susheel.thapa@outlook.com>
2021-06-29 11:24:10 +0545

    Comments are added for Program 6.1

--- Computer Lab/Lab 6 Source Code/Program06_01.c
+++ Computer Lab/Lab 6 Source Code/Program06_01.c
@@ -1,20 +1,32 @@
-/*2+4+6+8+10+....................+ 2n*/
+/*
+Question:
+    2+4+6+8+10+....................+ 2n
+
+*/
+
+/*Importing header files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 
 int main()
 {
+    /*Varaible declaration*/
     int n;
-    system("cls");
+    system("cls");/*CLear the screen*/
+
+    /*Taking input from user*/
     printf("Value of n: ");
     scanf("%d", &n);
     
-    printf("\nSequence is: \n\n");
+    printf("\nSequence is: \n\n");/*Information */
 
+    /*Simple for loop*/
     for (int i = 1; i <= n; i++)
     {
         printf("%d ", 2 * i);
+
+        /*Below if to print + but we donot want for at after last term so we break the loop when we have last term*/
         if (n == i)
         {
             break;
@@ -24,6 +36,20 @@ int main()
             printf("+ ");
         }
     }
+
     getch();
     return 0;
 }
+/*
+Logic of code:
+    The series is a purely a even number series
+    Problem lies in adding +
+        ~We can add + in print statement like
+            printf("%d + ",<value>);
+
+            but this will print the + after last term so we user and if condition
+
+        ~if last term is reached break the loop
+         if not, printf("+");
+
+*/
