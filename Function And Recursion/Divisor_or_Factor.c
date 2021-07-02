45544ba410555bb622d3698f10861ed0ace4885d
Susheel Thapa <susheel.thapa@outlook.com>
2021-07-02 12:43:53 +0545

    Kept the logic inside Function

--- Function And Recursion/Divisor_or_Factor.c
+++ Function And Recursion/Divisor_or_Factor.c
@@ -1,10 +1,14 @@
 /*
     Program to find the factors/divisors of the user input number
 */
+
+
 /*Including Header files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+
+
 /*
     Logic of code:
     Factors of the number are those number which exactly divides the number leave 0 reaminder.
@@ -13,23 +17,14 @@
     --> Check remainder is equals to 0 
         True: It is divisor/factor. And we just print it 
         False: It is not the divisior. We iterate over next number.
+
+    We keep all the logic in a function divisor_or_factor
 */
-int main()
+
+
+/*Function to calculate the divisor or factor of number*/
+void divisor(int number)
 {
-    /*Variable Declaration*/
-    int number;
-
-    system("cls"); /*Clear the terminal screen*/
-
-    /*Taking the user input*/
-    printf("Enter the postive integers whose factor you want to know: ");
-    scanf("%d", &number);
-
-    system("cls"); /*CLear the terminal*/
-
-    /*Main Code*/
-    printf("Number : %d\n\n", number); /*Prints the user input number*/
-
     /*Prints the factor/divisor in a good order*/
     printf("Factors/Divisors: ");
 
@@ -42,7 +37,27 @@ int main()
     }
     /*Above loop doesn't contain the user input number so keeping it at last using printf statement*/
     printf("%d\n", number);
+}
 
-    getch();
+
+int main()
+{
+    /*Variable Declaration*/
+    int number;
+
+    // system("cls"); /*Clear the terminal screen*/
+
+    /*Taking the user input*/
+    printf("Enter the postive integers whose factor you want to know: ");
+    scanf("%d", &number);
+
+    // system("cls"); /*CLear the terminal*/
+
+    /*Main Code*/
+    printf("Number : %d\n\n", number); /*Prints the user input number*/
+
+    divisor(number); /*Calling the function with value number*/
+
+    getch();/*Waits till a key is pressed*/
     return 0;
 }
