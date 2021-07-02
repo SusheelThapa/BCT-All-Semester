27bb291ba9c304f2174044cafed9c8473fbad2f1
Susheel Thapa <susheel.thapa@outlook.com>
2021-07-02 12:51:39 +0545

    Kept the logic inside Function

--- Function And Recursion/Greatest_Number.c
+++ Function And Recursion/Greatest_Number.c
@@ -1,7 +1,32 @@
 //Write a Program to find the greatest of four number entered by the user
+
+/*Importing header files*/
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+
+/*Function to calculate greatest number*/
+void greatest(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+{
+
+    //Checking the condition of greatest
+    if (firstNumber > secondNumber && firstNumber > thirdNumber && firstNumber > fourthNumber)
+    {
+        printf("\n%d is greatest number.", firstNumber);
+    }
+    else if (secondNumber > thirdNumber && secondNumber > fourthNumber)
+    {
+        printf("\n%d is greatest number.", secondNumber);
+    }
+    else if (thirdNumber > fourthNumber)
+    {
+        printf("\n%d is greatest number.", thirdNumber);
+    }
+    else
+    {
+        printf("\n%d is greatest number.", fourthNumber);
+    }
+}
 
 int main()
 {
@@ -11,36 +36,21 @@ int main()
     system("cls"); //Simply this is cls command run that is run in cmd.exe
 
     //Taking the input from the user
-    printf("Enter the firstnumber: ");
+    printf("Enter the first number: ");
     scanf("%d", &firstNumber);
 
-    printf("Enter the secondnumber: ");
+    printf("Enter the second number: ");
     scanf("%d", &secondNumber);
 
-    printf("Enter the thirdnumber: ");
+    printf("Enter the third number: ");
     scanf("%d", &thirdNumber);
 
-    printf("Enter the fourthnumber: ");
+    printf("Enter the fourth number: ");
     scanf("%d", &fourthNumber);
 
-    system("cls");
-    //Checking the condition of greatest
-    if (firstNumber > secondNumber && firstNumber > thirdNumber && firstNumber > fourthNumber)
-    {
-        printf("%d is greatest number.", firstNumber);
-    }
-    else if (secondNumber > thirdNumber && secondNumber > fourthNumber)
-    {
-        printf("%d is greatest number.", secondNumber);
-    }
-    else if (thirdNumber > fourthNumber)
-    {
-        printf("%d is greatest number.", thirdNumber);
-    }
-    else
-    {
-        printf("%d is greatest number.", fourthNumber);
-    }
+    /*Calling the function*/
+    greatest(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
     getch();
     return 0;
 }
