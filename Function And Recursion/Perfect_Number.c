a132289330ad1d1bab65733c5277f2cc644aeaf9
Susheel Thapa <susheel.thapa@outlook.com>
2021-07-02 13:04:12 +0545

    Kept logic of Program inside Function

--- Function And Recursion/Perfect_Number.c
+++ Function And Recursion/Perfect_Number.c
@@ -21,21 +21,10 @@ Then,
         False: It isn't perfect number
 */
 
-int main()
-{
-    /*Variable declaration*/
-    int number, sum_of_divisor_of_number;
+int sumOfDivisorOfNumber(int number){
 
-    system("cls");/*Clear the terminal */
-
-    /*Taking the user input*/
-    printf("Enter the number to check whether the number is Perfect or not: ");
-    scanf("%d", &number);
-
-    system("cls");/*CLeaing the terminal*/
-
-    
-    printf("Number: %d\n", number);/*Printing the orginal number*/
+    int sum_of_divisor_of_number= 0;
+    printf("Number: %d\n", number); /*Printing the orginal number*/
 
     /*Printing as well as adding divisor*/
     printf("Divisor: ");
@@ -50,9 +39,27 @@ int main()
             sum_of_divisor_of_number = sum_of_divisor_of_number + i;
         }
     }
-    printf("%d", number);/*Since it should not be added so we are adding this divisor seperaely*/
+    printf("%d\n", number); /*Since it should not be added so we are adding this divisor seperaely*/
 
-    printf("\nSum of Divisor(excluding itself): %d\n\n", sum_of_divisor_of_number);/*Printing the sum of divisor*/
+    return sum_of_divisor_of_number;
+}
+
+int main()
+{
+    /*Variable declaration*/
+    int number, sum_of_divisor_of_number;
+
+    system("cls");/*Clear the terminal */
+
+    /*Taking the user input*/
+    printf("Enter the number to check whether the number is Perfect or not: ");
+    scanf("%d", &number);
+
+    system("cls");/*CLeaing the terminal*/
+
+    /*Calling function**/
+    sum_of_divisor_of_number = sumOfDivisorOfNumber(number);
+
 
     /*Condition of Perfect Number*/
     if (sum_of_divisor_of_number == number)
