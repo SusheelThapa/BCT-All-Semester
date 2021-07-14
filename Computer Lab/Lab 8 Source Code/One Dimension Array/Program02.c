cc10fe6336fec6554422ddc242a9ac3d9e4c5be2
Susheel Thapa <susheel.thapa@outlook.com>
2021-07-14 13:46:00 +0545

    Program 2 One D Array

---
+++ Computer Lab/Lab 8 Source Code/One Dimension Array/Program02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, num[6];
+
+    system("cls");
+
+    printf("Enter the members of array: \n");
+    for (int i = 0; i < 6; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", num[i]);
+    }
+
+    getch();
+    return 0;
+}
