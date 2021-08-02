8cd28aa4b45361bc35393414c35926dfe9d995d0
Susheel Thapa <susheel.thapa@outlook.com>
2021-08-02 07:25:52 +0545

    Program 5 of Lab 10

---
+++ Computer Lab/Lab 10 Source Code/Program05.c
@@ -0,0 +1,87 @@
+/*
+Write a program to reverse the word using recursive function
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char string[], int length)
+{
+    "Reverse the string recursively based on length of string is even or odd";
+
+    /*Find the length of string and it is constant so it is stored in const integer variable*/
+    const int length_of_string = strlen(string);
+
+    /*Check length is odd or even as when have seperate process of reversing odd and even length of string*/
+    if (strlen(string) % 2 == 0)
+    {
+
+        /*Recursive termination condition*/
+        if (length == length_of_string / 2)
+        {
+            goto stop;
+        }
+        else
+        {
+            /*Swap the element of first and last position*/
+            char temp = string[length_of_string - length];
+            string[length_of_string - length] = string[length - 1];
+            string[length - 1] = temp;
+
+            /*Decrease length by 1*/
+            length = length - 1;
+
+            /*Function call*/
+            reverse(string, length);
+        }
+    }
+    else if (length_of_string % 2 == 1)
+    {
+        /*Finding midpoint as in odd length midpoint element isn't changed*/
+        const int midpoint = (length_of_string + 1) / 2;
+
+        /*Terminating condition*/
+        if (length == midpoint)
+        {
+            goto stop;
+        }
+        else
+        {
+            /*Swap the last and first element*/
+            char temp = string[length_of_string - length];
+            string[length_of_string - length] = string[length - 1];
+            string[length - 1] = temp;
+
+            /*Decrese length by 1*/
+            length = length - 1;
+
+            /*Function call*/
+            reverse(string, length);
+        }
+    }
+/*Label to program come if termination condition is acheived so that recursive function is terminated*/
+stop:;
+}
+
+int main()
+{
+    /*Variable declaration*/
+    char string[100];
+
+    system("cls");
+
+    /*Taking string from user*/
+    printf("String: ");
+    scanf("%[^'\n']", string);
+
+    /*Function call*/
+    reverse(string, strlen(string));
+
+    /*Reversing the string*/
+    printf("Reversed string is %s\n", string);
+
+    getch();
+    return 0;
+}
