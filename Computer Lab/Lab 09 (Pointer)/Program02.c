87ac85322842f3aa9290de6e71aa4bc6c9a82d81
Susheel Thapa <susheel.thapa@outlook.com>
2021-08-03 18:23:28 +0545

    Renamed folders, Delete unnecessary and Updated Comment in some files

--- Computer Lab/Lab 09 (Pointer)/Program02.c
+++ Computer Lab/Lab 09 (Pointer)/Program02.c
@@ -15,16 +15,21 @@ void largest(int a, int b, int *large)
 
 int main()
 {
+    /*Variable Declaration*/
     int number_one, number_two, large;
+
     system("cls");
 
+    /*Taking input from user*/
     printf("Number One: ");
     scanf("%d", &number_one);
     printf("Number Two: ");
     scanf("%d", &number_two);
 
+    /*Function call to find largest and store it in variable*/
     largest(number_one, number_two, &large);
 
+    /*Printing the result*/
     printf("Largest number is %d", large);
 
     getch();
--- Computer Lab/Lab 09 (Pointer)/Program04.c
+++ Computer Lab/Lab 09 (Pointer)/Program04.c
@@ -8,22 +8,27 @@ This program asks the required size of array to the user and displays the addres
 
 int main()
 {
+    /*Variable declaration*/
     int n, i;
     float *address; /*pointer variable declaration*/
 
     system("cls");
 
+    /*Asking the size of array*/
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
+    /*Creating a array with DMA*/
     address = (float *)calloc(n, sizeof(float)); /*Using calloc function to allocate the memory for n number of float number.*/
 
+    /*Checking if memory is allocated or not*/
     if (address == NULL)
     {
         printf("Memory is not allocated.\n");
         exit(0); /*to exit the program if contents of address is NULL.*/
     }
 
+    /*Printing the address of allocated blocks*/
     for (i = 0; i < n; i++)
     {
         printf("Address of %d block is %d.\n", i, (address + i));
