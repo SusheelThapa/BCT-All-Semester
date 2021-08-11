3b242b35fed86f576cfe33c01436a4bd37ae93cc
Susheel Thapa <susheel.thapa@outlook.com>
2021-08-11 11:36:27 +0545

    Logical Operator

---
+++ Logical_Operator.f95
@@ -0,0 +1,15 @@
+!Logical operator
+
+program Logical_operator
+    implicit none
+    ! .and. .or. .not. are the used logical operator in fortran
+
+    ! Built in
+        ! .true. == T 
+        ! .false. == F
+
+    print*, "True and False = ",.true. .and. .false.
+    print*, "True or False = ",.true. .or. .false.
+    print*, "Not of True = ",.not.(.true.)
+
+end program Logical_operator
