89fe418e018a80bc9c7ad50e4db97f94a6681007
Susheel Thapa <susheel.thapa@outlook.com>
2021-08-11 11:00:35 +0545

    Mathematical and Relational Operator

---
+++ Arithematic_Operation.f95
@@ -0,0 +1,21 @@
+
+program Arithematic_Operation
+    implicit none
+    !You can do any of the two
+    
+    !Method one
+        ! real :: a = 4.0,b =2.0
+    !Method two
+        real :: a,b
+        ! a = 4.0
+        ! b = 2.0
+
+        !assingning a and b can be done in one line
+        a = 4.0 ; b =2.0
+
+    print*, a+b
+    print*, a-b
+    print*, a*b
+    print*, a/b
+    print*, a**b
+end program Arithematic_Operation
