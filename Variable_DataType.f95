ce949d6c97bef8f11d738bab7ef93e56b00283ad
Susheel Thapa <susheel.thapa@outlook.com>
2021-08-11 10:28:11 +0545

    Making variable and assigning value

---
+++ Variable_DataType.f95
@@ -0,0 +1,18 @@
+program Variable_DataTypes
+    implicit none
+    !Variable Declaration
+    INTEGER ::x
+    REAL :: i
+    INTEGER :: c = 100!Assigning plus Declaration
+
+    !Assigning the value
+    x = 500
+    i = 4.98
+
+    Print*, 'x' ,x
+    Print*, 'i' ,i
+    Print*, 'c' ,c
+
+    ! Comment start with '!' in fortran
+
+end program Variable_DataTypes
