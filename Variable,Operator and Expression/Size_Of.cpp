8949eb34b2f46e942c2780a745c6310e387084d4
Susheel Thapa <susheel.thapa@outlook.com>
2021-10-09 21:20:00 +0545

    Size of Different Data Type in C++

---
+++ Variable,Operator and Expression/Size_Of.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "SIZE OF DIFFERENT DATA TYPE IN C++" << endl<< endl;
+    
+    cout << "Size of int " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float " << sizeof(float) << " bytes" << endl;
+    cout << "Size of char " << sizeof(char) << " bytes" << endl;
+    cout << "Size of double " << sizeof(double) << " bytes" << endl;
+    cout << "Size of bool " << sizeof(bool) << " bytes" << endl;
+
+    return 0;
+}
