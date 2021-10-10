7cf7a63a4dfb1b5eabddbd9d6fd971af2955045c
Susheel Thapa <susheel.thapa@outlook.com>
2021-10-10 13:27:22 +0545

    Call by Refernce Using Reference Varible
    Previous only one method was written. Here Call by reference using
    reference variable is shown with some commment that helps to understand
    the comment

--- Function/Call_By_Refrence.cpp
+++ Function/Call_By_Refrence.cpp
@@ -8,8 +8,16 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
+void swapRefVar(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main()
 {
+    //Call By Refrence Normal Method
     int a = 9, b = 10;
 
     cout << "Before Function call, value of a and b is " << a << " and " << b << endl;
@@ -24,6 +32,25 @@ int main()
 
     It is just like giving the location of orginal file in computer to you friend.
     And if he does any change in that then it will reflect in the orginal copy too(as orginal file has been changed)
+    */
+
+    //Call by Refrence Refrence Variable Method
+    int c = 90, d = 79;
+
+    cout << "Before Function call, value of a and b is " << c << " and " << d << endl;
+
+    swapRefVar(c, d);
+
+    cout << "After Function Call, value of a and b is " << c << " and " << d << endl;
+
+    /*
+    Here we can see the value has been changed.
+
+    As a person can be called by many name like mother call babu , father call chora and his friends call him Ram.
+    Then he some one punch on nose of babu , then punch is over chora and Ram as name is different but what they refer is same
+
+    In above program, both c and a refer same thing only name are different so change in a is reflect in c too. 
+    Same goes with d and b
     */
     return 0;
 }
