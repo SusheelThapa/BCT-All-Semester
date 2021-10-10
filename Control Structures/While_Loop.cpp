493e2023e4df4db3ffdce471d93ea4c65ef54670
Susheel Thapa <susheel.thapa@outlook.com>
2021-10-10 11:22:45 +0545

    While loop in C++

---
+++ Control Structures/While_Loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Syntax:
+        while(condition)
+        {
+            //Body
+        }
+    */
+
+    //Printing number from 1 to 100
+    int i = 1;
+
+    while (i <= 100)
+    {
+        cout << i << endl;
+        i++;
+    }
+    
+    return 0;
+}
